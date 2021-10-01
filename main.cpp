9a9b3cb18df62a3515d079d5c48cb0227b4e9d26
Denis Strizhkin <strdenis02@gmail.com>
2021-10-01 18:55:47 +0300

    added some testing

--- main.cpp
+++ main.cpp
@@ -9,5 +9,23 @@ float func(float num)
 
 int main()
 {
+  Matrix m;
 
+  std::cout << m.N() << '\n';
+  std::cout << m.M() << '\n';
+
+  double numbers[6] = { 0.5, 0.3, 0.4, 0.6, 0.8, 0.2 };
+  Matrix num_matrix(numbers, 3, 2);
+  num_matrix.Print();
+
+  for (unsigned i = 0; i < 10000000; i++)
+  {
+    std::cout << "i: " << i << '\n';
+    Matrix temp(10000, 10000);
+    Matrix a;
+
+    a = temp;
+
+    Matrix b = temp;
+  }
 }
