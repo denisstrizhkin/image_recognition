e720ac73fda9f02d358ae8f3bb68d2dfbdaa7dba
denis <strdenis02@gmail.com>
2021-10-29 11:16:04 +0300

    Added exceptions and 2d vector double constructor.

--- matrix.h
+++ matrix.h
@@ -7,20 +7,28 @@ class Matrix {
 private:
   size_t m_ = 0;
   size_t n_ = 0;
-  double* array_ = nullptr;
+  double *array_ = nullptr;
+
+  class WrongDimensions : public std::exception {
+    virtual const char *what() const noexcept;
+  };
+
+  class WrongIndex : public std::exception {
+    virtual const char *what() const noexcept;
+  };
 
 public:
   // Constructors
   Matrix();
   Matrix(size_t m, size_t n);
-  Matrix(const std::vector<double>& vector, size_t m, size_t n);
-  Matrix(const Matrix& matrix);
+  Matrix(const std::vector<double> &vector, size_t m, size_t n);
+  Matrix(const std::vector<std::vector<double>> &vector);
+  Matrix(const Matrix &matrix);
 
   // Destructor
   ~Matrix();
 
 };
-
 
 
 #endif //IMAGE_RECOGNITION_MATRIX_H
