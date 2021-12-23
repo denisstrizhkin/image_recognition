63f5985e0a42b1a0856507548e78b0229a4f956e
denis <strdenis02@gmail.com>
2021-12-23 11:44:07 +0300

    started code cleaning

--- include/matrix.h
+++ include/matrix.h
@@ -3,77 +3,67 @@
 
 #include <vector>
 
-class Matrix {
+class Mat {
  private:
   size_t m_ = 0;
   size_t n_ = 0;
-  double* array_ = nullptr;
-
-  class WrongDimensions : public std::exception {
-    [[nodiscard]] const char* what() const noexcept override;
-  };
-
-  class WrongIndex : public std::exception {
-    [[nodiscard]] const char* what() const noexcept override;
-  };
+  float* arr_ = nullptr;
 
  public:
   // Constructors
-  Matrix();
-  Matrix(size_t m, size_t n);
-  Matrix(const std::vector<double>& vector, size_t m, size_t n);
-  explicit Matrix(const std::vector<std::vector<double>>& vector);
-  Matrix(const Matrix& matrix);
+  Mat();
+  Mat(size_t m, size_t n);
+  Mat(const std::vector<float>& vec, size_t m, size_t n);
+  explicit Mat(const std::vector<std::vector<float>>& vec);
+  Mat(const Mat& mat);
 
   // Main operators overloading
-  Matrix& operator=(const Matrix& matrix);
-  Matrix& operator*=(const Matrix& matrix);
-  Matrix& operator+=(const Matrix& matrix);
-  Matrix& operator-=(const Matrix& matrix);
-  Matrix& operator/=(const Matrix& matrix);
+  Mat& operator =(const Mat& mat);
+  Mat& operator*=(const Mat& mat);
+  Mat& operator+=(const Mat& mat);
+  Mat& operator-=(const Mat& mat);
+  Mat& operator/=(const Mat& mat);
+
+  // Other operator overloading variants
+  // +
+  friend Mat operator+(const Mat& a, const Mat& b);
+  friend Mat operator+=(Mat& mat, float num);
+  friend Mat operator+(const Mat& mat, float num);
+  friend Mat operator+(float num, const Mat& mat);
+  // -
+  friend Mat operator-(const Mat& a, const Mat& b);
+  friend Mat operator-=(Mat& mat, float num);
+  friend Mat operator-(const Mat& mat, float num);
+  friend Mat operator-(float num, const Mat& mat);
+  // *
+  friend Mat operator*(const Mat& a, const Mat& b);
+  friend Mat operator*=(Mat& mat, float num);
+  friend Mat operator*(const Mat& mat, float num);
+  friend Mat operator*(float num, const Mat& mat);
+  // /
+  friend Mat operator/(const Mat& a, const Mat& b);
+  friend Mat operator/=(Mat& mat, float num);
+  friend Mat operator/(const Mat& mat, float num);
+  friend Mat operator/(float num, const Mat& mat);
+
+  // Other mat functions
+  void Print();
+  void Fill(float num);
+  [[nodiscard]] Mat ApplyFunc(float (*func)(float));
+  static void Randomize(Mat& mat);
 
   // Access functions
-  double& at(size_t i_m, size_t i_n);
-  [[nodiscard]] double at(size_t i_m, size_t i_n) const;
+  float& at(size_t m_i, size_t n_i);
+  [[nodiscard]] float at(size_t m_i, size_t n_i) const;
   [[nodiscard]] size_t m() const;
   [[nodiscard]] size_t n() const;
 
   // Other functions
-  [[nodiscard]] Matrix T() const;
-  [[nodiscard]] Matrix Dot(const Matrix& matrix) const;
-  [[nodiscard]] double Sum() const;
+  [[nodiscard]] Mat T() const;
+  [[nodiscard]] Mat Dot(const Mat& mat) const;
 
   // Destructor
-  ~Matrix();
+  ~Mat();
 };
-
-// Other operator overloading variants
-// +
-Matrix operator+(const Matrix& matrix_1, const Matrix& matrix_2);
-Matrix operator+=(Matrix& matrix, double num);
-Matrix operator+(const Matrix& matrix, double num);
-Matrix operator+(double num, const Matrix& matrix);
-// -
-Matrix operator-(const Matrix& matrix_1, const Matrix& matrix_2);
-Matrix operator-=(Matrix& matrix, double num);
-Matrix operator-(const Matrix& matrix, double num);
-Matrix operator-(double num, const Matrix& matrix);
-// *
-Matrix operator*(const Matrix& matrix_1, const Matrix& matrix_2);
-Matrix operator*=(Matrix& matrix, double num);
-Matrix operator*(const Matrix& matrix, double num);
-Matrix operator*(double num, const Matrix& matrix);
-
-// /
-Matrix operator/(const Matrix& matrix_1, const Matrix& matrix_2);
-Matrix operator/=(Matrix& matrix, double num);
-Matrix operator/(const Matrix& matrix, double num);
-Matrix operator/(double num, const Matrix& matrix);
-
-// Other matrix functions
-void PrintMatrix(const Matrix& matrix);
-void FillMatrix(Matrix& matrix, double num);
-Matrix ApplyFunc(const Matrix& matrix, double (*func)(double));
-void RandomizeMatrix(Matrix& matrix);
 
 #endif  // IMAGE_RECOGNITION_MATRIX_H_
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,247 +1,259 @@
 #include "matrix.h"
 
 #include <chrono>
+#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <random>
+#include <stdexcept>
+#include <algorithm>
 
-Matrix::Matrix() = default;
-Matrix::~Matrix() { delete[] array_; }
-size_t Matrix::m() const { return m_; }
-size_t Matrix::n() const { return n_; }
-
-Matrix::Matrix(size_t m, size_t n) {
-  m_ = m;
-  n_ = n;
-  array_ = new double[m_ * n_];
+void ThrowDim() {
+  throw std::invalid_argument("Dimensions do not match!");
 }
 
-Matrix::Matrix(const std::vector<double>& vector, size_t m, size_t n)
-    : Matrix(m, n) {
-  if (vector.size() != m_ * n_) throw WrongDimensions();
-  for (size_t i = 0; i < m_ * n_; i++) array_[i] = vector.at(i);
+void DimensionsMatch(const Mat& a, const Mat& b) {
+  if (a.m() != b.m() || b.n() != a.n())
+    ThrowDim();
 }
 
-Matrix::Matrix(const std::vector<std::vector<double>>& vector)
-    : Matrix(vector.size(), vector.at(0).size()) {
-  for (size_t i_m = 0; i_m < m_; i_m++) {
-    for (size_t i_n = 0; i_n < n_; i_n++) {
-      array_[i_m * n_ + i_n] = vector.at(i_m).at(i_n);
-    }
-  }
+Mat::Mat() = default;
+Mat::~Mat() { delete[] arr_; }
+size_t Mat::m() const { return m_; }
+size_t Mat::n() const { return n_; }
+
+Mat::Mat(size_t m, size_t n) : m_(m), n_(n) {
+  arr_ = new float[m_ * n_];
 }
 
-Matrix::Matrix(const Matrix& matrix) : Matrix(matrix.m_, matrix.n_) {
-  for (size_t i = 0; i < m_ * n_; i++) this->array_[i] = matrix.array_[i];
+Mat::Mat(const std::vector<float>& vec, size_t m, size_t n)
+    : Mat(m, n) {
+  if (vec.size() != m_ * n_) ThrowDim();
+
+  std::copy(vec.begin(), vec.end(), arr_);
 }
 
-Matrix& Matrix::operator=(const Matrix& matrix) {
-  if (this == &matrix) return *this;
-  delete[] this->array_;
-  this->m_ = matrix.m_;
-  this->n_ = matrix.n_;
-  this->array_ = new double[this->m_ * this->n_];
-  for (size_t i = 0; i < m_ * n_; i++) this->array_[i] = matrix.array_[i];
+Mat::Mat(const std::vector<std::vector<float>>& vec)
+    : Mat(vec.size(), vec.at(0).size()) {
+  for (size_t m_i = 0; m_i < m_; m_i++)
+    std::copy(vec[m_i].begin(), vec[m_i].end(), arr_ + m_i*n_);
+}
+
+Mat::Mat(const Mat& mat) : Mat(mat.m_, mat.n_) {
+  std::copy(&mat.arr_[0], &mat.arr_[mat.m_*mat.n_], this->arr_);
+}
+
+Mat& Mat::operator=(const Mat& mat) {
+  if (this == &mat) return *this;
+
+  delete[] this->arr_;
+
+  this->m_ = mat.m_;
+  this->n_ = mat.n_;
+
+  this->arr_ = new float[this->m_ * this->n_];
+  std::copy(&mat.arr_[0], &mat.arr_[mat.m_*mat.n_], this->arr_);
+ 
   return *this;
 }
 
-Matrix& Matrix::operator+=(const Matrix& matrix) {
-  if (this->m_ != matrix.m_ || this->n_ != matrix.n_) throw WrongDimensions();
-  for (size_t i = 0; i < this->m_ * this->n_; i++) {
-    this->array_[i] += matrix.array_[i];
-  }
+Mat& Mat::operator+=(const Mat& mat) {
+  DimensionsMatch(mat, *this); 
+
+  std::transform(&arr_[0], &arr_[m_*n_],
+      &mat.arr_[0], &mat.arr_[m_*n_], std::plus<float>());
+
   return *this;
 }
 
-Matrix& Matrix::operator*=(const Matrix& matrix) {
-  if (this->m_ != matrix.m_ || this->n_ != matrix.n_) throw WrongDimensions();
-  for (size_t i = 0; i < this->m_ * this->n_; i++) {
-    this->array_[i] *= matrix.array_[i];
-  }
+Mat& Mat::operator*=(const Mat& mat) {
+  DimensionsMatch(mat, *this); 
+
+  std::transform(&arr_[0], &arr_[m_*n_],
+      &mat.arr_[0], &mat.arr_[m_*n_], std::multiplies<float>());
+
   return *this;
 }
 
-Matrix& Matrix::operator/=(const Matrix& matrix) {
-  if (this->m_ != matrix.m_ || this->n_ != matrix.n_) throw WrongDimensions();
-  for (size_t i = 0; i < this->m_ * this->n_; i++) {
-    this->array_[i] /= matrix.array_[i];
-  }
+Mat& Mat::operator/=(const Mat& mat) {
+  DimensionsMatch(mat, *this); 
+
+  std::transform(&arr_[0], &arr_[m_*n_],
+      &mat.arr_[0], &mat.arr_[m_*n_], std::divides<float>());
+
   return *this;
 }
 
-Matrix& Matrix::operator-=(const Matrix& matrix) {
-  if (this->m_ != matrix.m_ || this->n_ != matrix.n_) throw WrongDimensions();
-  for (size_t i = 0; i < this->m_ * this->n_; i++) {
-    this->array_[i] -= matrix.array_[i];
-  }
+Mat& Mat::operator-=(const Mat& mat) {
+  DimensionsMatch(mat, *this); 
+
+  std::transform(&arr_[0], &arr_[m_*n_],
+      &mat.arr_[0], &mat.arr_[m_*n_], std::minus<float>());
+
   return *this;
 }
 
-double Matrix::at(size_t i_m, size_t i_n) const {
-  if (i_m >= m_ || i_n >= n_) throw WrongIndex();
-  return array_[i_m * n_ + i_n];
+float Mat::at(size_t m_i, size_t n_i) const {
+  if (m_i >= m_ || n_i >= n_) throw std::out_of_range("Wrong Index!");
+  return arr_[m_i * n_ + n_i];
 }
 
-double& Matrix::at(size_t i_m, size_t i_n) {
-  if (i_m >= m_ || i_n >= n_) throw WrongIndex();
-  return array_[i_m * n_ + i_n];
+float& Mat::at(size_t m_i, size_t n_i) {
+  if (m_i >= m_ || n_i >= n_) throw std::out_of_range("Wrong Index!");
+  return arr_[m_i * n_ + n_i];
 }
 
-void FillMatrix(Matrix& matrix, double num) {
-  for (size_t i_m = 0; i_m < matrix.m(); i_m++) {
-    for (size_t i_n = 0; i_n < matrix.n(); i_n++) matrix.at(i_m, i_n) = num;
-  }
+void Mat::Fill(float num) {
+  std::fill(&arr_[0], &arr_[m_*n_], num);
 }
 
-void PrintMatrix(const Matrix& matrix) {
-  std::cout << "--- m(" << matrix.m() << "):n(" << matrix.n() << ")\n";
-  for (size_t i_m = 0; i_m < matrix.m(); i_m++) {
-    for (size_t i_n = 0; i_n < matrix.n(); i_n++) {
-      std::cout << std::setw(8) << matrix.at(i_m, i_n);
-    }
+void Mat::Print() {
+  std::cout << "--- m(" << m_ << "):n(" << n_ << ")\n";
+  for (float* p = &arr_[0]; p < &arr_[m_*n_]; p+=n_) {
+    std::for_each(p, p+n_, [](float n){ std::cout << std::setw(8) << n; });
     std::cout << '\n';
   }
   std::cout << "---\n";
 }
 
-void RandomizeMatrix(Matrix& matrix) {
-  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-  std::default_random_engine rnd_engine(seed);
-  std::uniform_real_distribution<double> distribution(0.0, 1.0);
-  for (size_t i_m = 0; i_m < matrix.m(); i_m++) {
-    for (size_t i_n = 0; i_n < matrix.n(); i_n++) {
-      matrix.at(i_m, i_n) = distribution(rnd_engine);
-    }
-  }
-}
-
-Matrix ApplyFunc(const Matrix& matrix, double (*func)(double)) {
-  Matrix result = matrix;
-  for (size_t i_m = 0; i_m < matrix.m(); i_m++) {
-    for (size_t i_n = 0; i_n < matrix.n(); i_n++) {
-      result.at(i_m, i_n) = func(matrix.at(i_m, i_n));
-    }
-  }
-  return result;
-}
-
-Matrix Matrix::Dot(const Matrix& matrix) const {
-  if (this->n_ != matrix.m_) throw WrongDimensions();
-  Matrix dot_result(this->m_, matrix.n_);
-  for (size_t i_new_m = 0; i_new_m < dot_result.m_; i_new_m++) {
-    for (size_t i_new_n = 0; i_new_n < dot_result.n_; i_new_n++) {
-      size_t i_new = i_new_m * dot_result.n_ + i_new_n;
-      dot_result.array_[i_new] = 0.0;
-      for (size_t i_n = 0; i_n < n_; i_n++) {
-        dot_result.array_[i_new] += array_[i_new_m * n_ + i_n] *
-                                    matrix.array_[i_n * matrix.n_ + i_new_n];
-      }
-    }
-  }
-  return dot_result;
-}
-
-Matrix Matrix::T() const {
-  Matrix result(n_, m_);
-  for (size_t i_m = 0; i_m < m_; i_m++) {
-    for (size_t i_n = 0; i_n < n_; i_n++) {
-      result.array_[i_n * m_ + i_m] = array_[i_m * n_ + i_n];
-    }
-  }
-  return result;
-}
-
-double Matrix::Sum() const {
-  double sum = 0.0;
-  for (size_t i = 0; i < m_ * n_; i++) sum += array_[i];
-  return sum;
-}
-
-Matrix operator+(const Matrix& matrix_1, const Matrix& matrix_2) {
-  Matrix tmp_matrix_1 = matrix_1;
-  return tmp_matrix_1 += matrix_2;
-}
-
-Matrix operator+=(Matrix& matrix, double num) {
-  Matrix num_matrix(matrix.m(), matrix.n());
-  FillMatrix(num_matrix, num);
-  return matrix += num_matrix;
-}
-
-Matrix operator+(const Matrix& matrix, double num) {
-  Matrix num_matrix(matrix.m(), matrix.n());
-  FillMatrix(num_matrix, num);
-  return matrix + num_matrix;
-}
-
-Matrix operator+(double num, const Matrix& matrix) {
-  return operator+(matrix, num);
-}
-
-Matrix operator-(const Matrix& matrix_1, const Matrix& matrix_2) {
-  Matrix tmp_matrix_1 = matrix_1;
-  return tmp_matrix_1 -= matrix_2;
-}
-
-Matrix operator-=(Matrix& matrix, double num) {
-  Matrix num_matrix(matrix.m(), matrix.n());
-  FillMatrix(num_matrix, num);
-  return matrix -= num_matrix;
-}
-
-Matrix operator-(const Matrix& matrix, double num) {
-  Matrix num_matrix(matrix.m(), matrix.n());
-  FillMatrix(num_matrix, num);
-  return matrix - num_matrix;
-}
-
-Matrix operator-(double num, const Matrix& matrix) {
-  return operator-(matrix, num);
-}
-
-Matrix operator*(const Matrix& matrix_1, const Matrix& matrix_2) {
-  Matrix tmp_matrix_1 = matrix_1;
-  return tmp_matrix_1 *= matrix_2;
-}
-
-Matrix operator*=(Matrix& matrix, double num) {
-  Matrix num_matrix(matrix.m(), matrix.n());
-  FillMatrix(num_matrix, num);
-  return matrix *= num_matrix;
-}
-
-Matrix operator*(const Matrix& matrix, double num) {
-  Matrix num_matrix(matrix.m(), matrix.n());
-  FillMatrix(num_matrix, num);
-  return matrix * num_matrix;
-}
-
-Matrix operator*(double num, const Matrix& matrix) {
-  return operator*(matrix, num);
-}
-
-Matrix operator/(const Matrix& matrix_1, const Matrix& matrix_2) {
-  Matrix tmp_matrix_1 = matrix_1;
-  return tmp_matrix_1 /= matrix_2;
-}
-
-Matrix operator/=(Matrix& matrix, double num) {
-  Matrix num_matrix(matrix.m(), matrix.n());
-  FillMatrix(num_matrix, num);
-  return matrix /= num_matrix;
-}
-
-Matrix operator/(const Matrix& matrix, double num) {
-  Matrix num_matrix(matrix.m(), matrix.n());
-  FillMatrix(num_matrix, num);
-  return matrix / num_matrix;
-}
-
-Matrix operator/(double num, const Matrix& matrix) {
-  return operator/(matrix, num);
-}
-
-const char* Matrix::WrongDimensions::what() const noexcept {
-  return "Dimensions do not match!";
-}
-const char* Matrix::WrongIndex::what() const noexcept { return "Wrong Index!"; }
+//void RandomizeMat(Mat& mat) {
+//  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+//  std::default_random_engine rnd_engine(seed);
+//  std::uniform_real_distribution<float> distribution(0.0, 1.0);
+//  for (size_t i_m = 0; i_m < mat.m(); i_m++) {
+//    for (size_t i_n = 0; i_n < mat.n(); i_n++) {
+//      mat.at(i_m, i_n) = distribution(rnd_engine);
+//    }
+//  }
+//}
+//
+//Mat ApplyFunc(const Mat& mat, float (*func)(float)) {
+//  Mat result = mat;
+//  for (size_t i_m = 0; i_m < mat.m(); i_m++) {
+//    for (size_t i_n = 0; i_n < mat.n(); i_n++) {
+//      result.at(i_m, i_n) = func(mat.at(i_m, i_n));
+//    }
+//  }
+//  return result;
+//}
+//
+//Mat Mat::Dot(const Mat& mat) const {
+//  if (this->n_ != mat.m_) throw WrongDimensions();
+//  Mat dot_result(this->m_, mat.n_);
+//  for (size_t i_new_m = 0; i_new_m < dot_result.m_; i_new_m++) {
+//    for (size_t i_new_n = 0; i_new_n < dot_result.n_; i_new_n++) {
+//      size_t i_new = i_new_m * dot_result.n_ + i_new_n;
+//      dot_result.arr_[i_new] = 0.0;
+//      for (size_t i_n = 0; i_n < n_; i_n++) {
+//        dot_result.arr_[i_new] += arr_[i_new_m * n_ + i_n] *
+//                                    mat.arr_[i_n * mat.n_ + i_new_n];
+//      }
+//    }
+//  }
+//  return dot_result;
+//}
+//
+//Mat Mat::T() const {
+//  Mat result(n_, m_);
+//  for (size_t i_m = 0; i_m < m_; i_m++) {
+//    for (size_t i_n = 0; i_n < n_; i_n++) {
+//      result.arr_[i_n * m_ + i_m] = arr_[i_m * n_ + i_n];
+//    }
+//  }
+//  return result;
+//}
+//
+//float Mat::Sum() const {
+//  float sum = 0.0;
+//  for (size_t i = 0; i < m_ * n_; i++) sum += arr_[i];
+//  return sum;
+//}
+//
+//Mat operator+(const Mat& mat_1, const Mat& mat_2) {
+//  Mat tmp_mat_1 = mat_1;
+//  return tmp_mat_1 += mat_2;
+//}
+//
+//Mat operator+=(Mat& mat, float num) {
+//  Mat num_mat(mat.m(), mat.n());
+//  FillMat(num_mat, num);
+//  return mat += num_mat;
+//}
+//
+//Mat operator+(const Mat& mat, float num) {
+//  Mat num_mat(mat.m(), mat.n());
+//  FillMat(num_mat, num);
+//  return mat + num_mat;
+//}
+//
+//Mat operator+(float num, const Mat& mat) {
+//  return operator+(mat, num);
+//}
+//
+//Mat operator-(const Mat& mat_1, const Mat& mat_2) {
+//  Mat tmp_mat_1 = mat_1;
+//  return tmp_mat_1 -= mat_2;
+//}
+//
+//Mat operator-=(Mat& mat, float num) {
+//  Mat num_mat(mat.m(), mat.n());
+//  FillMat(num_mat, num);
+//  return mat -= num_mat;
+//}
+//
+//Mat operator-(const Mat& mat, float num) {
+//  Mat num_mat(mat.m(), mat.n());
+//  FillMat(num_mat, num);
+//  return mat - num_mat;
+//}
+//
+//Mat operator-(float num, const Mat& mat) {
+//  return operator-(mat, num);
+//}
+//
+//Mat operator*(const Mat& mat_1, const Mat& mat_2) {
+//  Mat tmp_mat_1 = mat_1;
+//  return tmp_mat_1 *= mat_2;
+//}
+//
+//Mat operator*=(Mat& mat, float num) {
+//  Mat num_mat(mat.m(), mat.n());
+//  FillMat(num_mat, num);
+//  return mat *= num_mat;
+//}
+//
+//Mat operator*(const Mat& mat, float num) {
+//  Mat num_mat(mat.m(), mat.n());
+//  FillMat(num_mat, num);
+//  return mat * num_mat;
+//}
+//
+//Mat operator*(float num, const Mat& mat) {
+//  return operator*(mat, num);
+//}
+//
+//Mat operator/(const Mat& mat_1, const Mat& mat_2) {
+//  Mat tmp_mat_1 = mat_1;
+//  return tmp_mat_1 /= mat_2;
+//}
+//
+//Mat operator/=(Mat& mat, float num) {
+//  Mat num_mat(mat.m(), mat.n());
+//  FillMat(num_mat, num);
+//  return mat /= num_mat;
+//}
+//
+//Mat operator/(const Mat& mat, float num) {
+//  Mat num_mat(mat.m(), mat.n());
+//  FillMat(num_mat, num);
+//  return mat / num_mat;
+//}
+//
+//Mat operator/(float num, const Mat& mat) {
+//  return operator/(mat, num);
+//}
+//
+//const char* Mat::WrongDimensions::what() const noexcept {
+//  return "Dimensions do not match!";
+//}
+//const char* Mat::WrongIndex::what() const noexcept { return "Wrong Index!"; }
